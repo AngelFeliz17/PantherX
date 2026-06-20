64337b37f6b7491286d01472681aefc22f7a26a0
Angel Feliz <52576090+AngelFeliz17@users.noreply.github.com>
2026-06-20 11:16:47 -0500

    Changed Admin Route

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -4,11 +4,12 @@ import { CategoryService } from './category.service';
 import { JwtGuard } from 'src/auth/guard';
 import { AdminGuard } from 'src/user/guard';
 
-@UseGuards(JwtGuard, AdminGuard)
+@UseGuards(JwtGuard)
 @Controller('categories')
 export class CategoryController {
     constructor(private categoryService: CategoryService) {}
 
+    @UseGuards(AdminGuard)
     @Post()
     create(@Body() dto: CategoryDto) {
         return this.categoryService.create(dto);
@@ -19,16 +20,19 @@ export class CategoryController {
         return this.categoryService.findAll()
     }
 
+    @UseGuards(AdminGuard)
     @Get(':id')
     find(@Param('id') id: string) {
         return this.categoryService.find(id);
     }
 
+    @UseGuards(AdminGuard)
     @Patch(':id')
     update(@Param('id') id: string, @Body() dto: UpdateCategoryDto) {
         return this.categoryService.update(id, dto);
     }
 
+    @UseGuards(AdminGuard)
     @Delete(':id')
     delete(@Param('id') id: string) {
         return this.categoryService.delete(id);
